8698a0e1f6a35703251ade18180a5e8c4faf9f94
Rostyk Machikha <finessq@gmail.com>
2018-08-17 14:55:50 +0300

    fix max discount

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,13 +1,15 @@
 let price = prompt('Enter a price');
 let discount = prompt('Enter discount');
-if (price < 0 || discount < 0) {
+if (price < 0 || discount < 0 || discount > 100) {
 	console.error('Invalid data');
-} else {
+}else {
+	let priceDiscount = price - price * discount / 100;
+	let saved = price * discount / 100;
 	console.log(`
 Price without discount: ${price}
 Discount: ${discount}%
-Price with discount: ${price - price * discount/100}
-Saved: ${price * discount / 100}`);
+Price with discount: ${+priceDiscount.toFixed(2)}
+Saved: ${+saved.toFixed(2)}`);
 }
 
 
